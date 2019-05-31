51dcc1d1aae24b7283dac6c8b8d835faa14900d0
klau5 <45886598+klau5@users.noreply.github.com>
2019-05-31 22:09:00 +0530

    installed bcrypt

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex') 
+const bcrypt = require('bcrypt');
+
+/// Knex:- DB handler
+/// Remember to change User when switching platforms
 
 const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
-        user: 'klau5',
+        user: 'postgres',   /// klau5 on Mac and postgres on Windows
         password: '',
         database: 'face_recog'
     }
@@ -45,6 +49,8 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
+/// signin existing user
+
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
@@ -54,8 +60,11 @@ app.post('/signin', (req, res) => {
     }
 })
 
+/// Register a new user
+
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password);
     db('users').returning('*').insert({
        name: name,
        email: email,
@@ -67,32 +76,36 @@ app.post('/register', (req, res) => {
      .catch(err => res.status(400).json('Unable to register'));
 })
 
+/// search user by :id
+
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params; 
     let found = false;
     db.select('*').from('users').where({id})
     .then(user => {
-        console.log(user);
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('User not found')
+        } 
     })
-    if (!found) {
-        res.status(400).json('User not found');
-    }
+    .catch(err => res.status(400).json('Erro getting user'))
 })
 
+/// Update entries based on number of images submitted
+
 app.put('/image', (req, res) => {
-    const {id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]); 
     })
-    if (!found) {
-        res.status(400).json('entry not found');
-    }
-})
+    .catch(err => res.status(400).json('Unable to get entries'))
+}) 
+
+/// check if server is running
 
 app.listen(3000, () => {
     console.log('app is running');
