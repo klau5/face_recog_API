2f7c52072cf49d03e3fce4367eacd26a054f8834
klau5_dev <jografie@gmail.com>
2019-05-31 20:46:57 +0530

    knex, pg, nodemon update

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex') 
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'klau5',
+        password: '',
+        database: 'face_recog'
+    }
+});
+
+db.select('*').from('users');
 
 const app = express();
 
@@ -43,25 +56,23 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    database.users.push({
-        id: '234',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+    db('users').returning('*').insert({
+       name: name,
+       email: email,
+       joined: new Date()
     })
-    res.json(database.users[database.users.length-1])
+     .then(response => {
+        res.json(response);
+     })
+     .catch(err => res.status(400).json('Unable to register'));
 })
 
 app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
+    const { id } = req.params; 
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
+    db.select('*').from('users').where({id})
+    .then(user => {
+        console.log(user);
     })
     if (!found) {
         res.status(400).json('User not found');
